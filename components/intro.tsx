f1a60c710efc2791bdf69a2989fb5f2bd7ae14cd
Burcu Bayram <burju7@yahoo.com>
2023-08-26 18:02:07 +0300

    Feat:dark mode implemented

--- components/intro.tsx
+++ components/intro.tsx
@@ -108,7 +108,7 @@ export default function Intro() {
         <a
           href="/BurcuBayram_CV.pdf"
           download
-          className="group bg-white px-7 py-3 flex item-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition borderBlack"
+          className="group bg-white px-7 py-3 flex item-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition borderBlack dark:bg-white/10"
         >
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
@@ -116,14 +116,14 @@ export default function Intro() {
         <a
           href="https://cut.lu/bnyo65"
           target="_blank"
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition borderBlack"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60"
         >
           <BsLinkedin />
         </a>
         <a
           href="https://cut.lu/y7r9at"
           target="_blank"
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-[1.15] transition borderBlack"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-[1.15] transition borderBlack dark:bg-white/10 dark:text-white/60"
         >
           <FaGithubSquare />
         </a>
--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 export default function SectionDivider() {
   return (
     <motion.div
-      className="bg-[#7C3E66]/50 my-24 h-16 w-1 rounded-full hidden sm:block"
+      className="bg-[#7C3E66]/50 my-24 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-20"
       initial={{
         opacity: 0,
         y: 100,
