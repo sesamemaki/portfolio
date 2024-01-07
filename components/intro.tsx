751cdb0bec92227ebcc47c1365dbca0bb8dc9024
Burcu Bayram <burju7@yahoo.com>
2024-01-08 00:21:11 +0300

    renewed Cv download

--- components/intro.tsx
+++ components/intro.tsx
@@ -104,7 +104,7 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
         <a
-          href="/BurcuBayram_CV-2023.9.19.pdf"
+          href="/BurcuBayram_CV-2023.11.22.pdf"
           download
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition borderBlack dark:bg-white/10"
         >
